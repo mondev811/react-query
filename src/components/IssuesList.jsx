75c7bd205db0e0197e6390e10420608589096738
Monica Rivera Pun <mondev811@gmail.com>
2023-01-09 23:39:28 -0800

    Add issues query

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,47 @@
 import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+
+const IssueItem = ({
+  title,
+  number,
+  assignee,
+  commentCount,
+  createdBy,
+  createdDate,
+  labels,
+  status,
+}) => {
+  return <li>{title}</li>;
+};
 
 export default function IssuesList() {
+  const issuesQuery = useQuery(["issues"], () =>
+    fetch("/api/issues").then((res) => res.json())
+  );
   return (
     <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      <h2>Issues List</h2>
+      {issuesQuery.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="issues-list">
+          {issuesQuery.data.map((item) => {
+            return (
+              <IssueItem
+                key={item.id}
+                title={item.title}
+                number={item.number}
+                assignee={item.assignee}
+                commentCount={item.comments.length}
+                createdBy={item.createdBy}
+                createdDate={item.createdDate}
+                labels={item.labels}
+                status={item.status}
+              />
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
