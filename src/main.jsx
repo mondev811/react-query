f878df133aa0854174a086163087084cf1ba3282
Monica Rivera Pun <mondev811@gmail.com>
2023-01-12 00:45:48 -0800

    Update react-query, install ReactQueryDevTools

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { worker } from "@uidotdev/react-query-api";
-import { QueryClient, QueryClientProvider } from "react-query";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 
 new Promise((res) => setTimeout(res, 100))
@@ -24,6 +24,7 @@ new Promise((res) => setTimeout(res, 100))
               <App />
             </div>
           </BrowserRouter>
+          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </React.StrictMode>,
       document.getElementById("root")
