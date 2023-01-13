d545d965f834f099a10f056ac256a1e4617fabb7
Monica Rivera Pun <mondev811@gmail.com>
2023-01-12 22:49:19 -0800

    Update avatars and user list

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -1,5 +1,6 @@
 import { GoComment, GoIssueOpened } from "react-icons/go";
 import { relativeDate } from "../helpers/relativeDate";
+import { useUserData } from "../helpers/useUserData";
 
 export const IssueItem = ({
   title,
@@ -11,6 +12,8 @@ export const IssueItem = ({
   labels,
   status,
 }) => {
+  const assigneeUser = useUserData(assignee);
+  const createdByUser = useUserData(createdBy);
   return (
     <li>
       <div>
@@ -22,17 +25,25 @@ export const IssueItem = ({
       </div>
       <div className="issue-content">
         <span>
-          <a href="">{title}</a>
+          <a href={`/issue/${number}`}>{title}</a>
           {labels.map((label) => (
             <span key={label} className={`label red`}>
               {label}
             </span>
           ))}
         </span>
-        <small>
-          #{number} opened {relativeDate(createdDate)} by {createdBy}
-        </small>
+        <small>{`#${number} opened ${relativeDate(createdDate)} by 
+       ${createdByUser.isSuccess ? createdByUser.data.name : ""}`}</small>
       </div>
+      {assignee &&
+        assigneeUser.isSuccess &&
+        assigneeUser.data.profilePictureUrl && (
+          <img
+            className="assigned-to"
+            src={assigneeUser.data.profilePictureUrl}
+            alt="Assigned to avatar"
+          />
+        )}
       <span className="comment-count">
         <GoComment />
         {commentCount}
