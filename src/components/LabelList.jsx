269943565bb060eb1d4d0eca37d2dfbeaaea1a72
Monica Rivera Pun <mondev811@gmail.com>
2023-01-14 17:23:14 -0800

    Add side label list

--- src/components/LabelList.jsx
+++ src/components/LabelList.jsx
@@ -1,3 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+
 export default function LabelList() {
-  return <h3>Labels</h3>;
+  const labelsQuery = useQuery(["labels"], () =>
+    fetch("/api/labels").then((res) => res.json())
+  );
+
+  return (
+    <>
+      {labelsQuery.isLoading && <p>Loading labels...</p>}
+      {labelsQuery.isSuccess && (
+        <div className="labels">
+          <h3>Labels</h3>
+          <ul>
+            {labelsQuery.data.map((label) => {
+              return (
+                <li key={label.id}>
+                  <button className={label.color}>{label.name}</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
