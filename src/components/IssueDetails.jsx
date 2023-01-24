8dc36ae8dcc0259f4866a229ae768be8e3be5ee5
Monica Rivera Pun <mondev811@gmail.com>
2023-01-23 23:46:46 -0800

    Add header to issue details page

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,7 +1,71 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import { relativeDate } from "../helpers/relativeDate";
 
+const useIssueData = (issueNumber) => {
+  return useQuery(["issues", issueNumber], () => {
+    return fetch(`/api/issues/${issueNumber}`).then((res) => res.json());
+  });
+};
+
+const Comment = ({ issueNumber, commentId }) => {
+  console.log(commentId);
+  const commentsQuery = useQuery(["issues", issueNumber, "comments"], () => {
+    return fetch(`/api/issues/${issueNumber}/comments`).then((res) =>
+      res.json()
+    );
+  });
+
+  if (commentsQuery.isLoading) return <p>Loading comments...</p>;
+  if (commentsQuery.isSuccess) {
+    const comment = commentsQuery.data.find((c) => c.id === commentId);
+    console.log(commentsQuery.data);
+    return (
+      <div className="comment">
+        <img src="" alt="Commenter avatar" />
+        <div>
+          <div className="comment-header"></div>
+          <div className="comment-body">{comment && comment.comment}</div>
+        </div>
+      </div>
+    );
+  }
+};
 export default function IssueDetails() {
   const { number } = useParams();
+  const { data, isLoading, isSuccess } = useIssueData(number);
 
-  return <h1>Issue {number}</h1>;
+  if (isLoading) return <p>Loading...</p>;
+  if (isSuccess)
+    return (
+      <div className="issue-details">
+        <header>
+          <h2>
+            {data.title}
+            <span>{` # ${data.number}`}</span>
+          </h2>
+          <div>
+            <span className="open">{data.status}</span>
+            <span className="created-by">{data.createdBy}</span>
+            {"opened this issue "}
+            {relativeDate(data.createdDate)}
+            {" . "}
+            {data.comments.length}
+            {" comments"}
+          </div>
+        </header>
+        <main>
+          <section>
+            {/* {data.comments.map((commentId) => ( */}
+            <Comment
+              issueNumber={number}
+              commentId={data.comments[0]}
+              key={data.comments[0]}
+            />
+            {/* ))} */}
+          </section>
+        </main>
+      </div>
+      // }
+    );
 }
