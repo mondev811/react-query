ae3db974c2e71c2261c5eef94415c88efe4fda30
Monica Rivera Pun <mondev811@gmail.com>
2023-01-25 22:02:28 -0800

    Finish IssueHeader

--- src/components/IssueHeader.jsx
+++ src/components/IssueHeader.jsx
@@ -1,13 +1,17 @@
+import { GoIssueOpened, GoIssueClosed } from "react-icons/go";
+import { possibleStatus } from "../helpers/defaultData";
+import { useUserData } from "../helpers/queryhooks";
 import { relativeDate } from "../helpers/relativeDate";
 
 export const IssueHeader = ({
   title,
   number,
-  status,
+  status = "todo",
   createdBy,
   createdDate,
   comments,
 }) => {
+  const createdByUser = useUserData(createdBy);
   return (
     <header>
       <h2>
@@ -15,11 +19,24 @@ export const IssueHeader = ({
         <span>{` # ${number}`}</span>
       </h2>
       <div>
-        <span className="open">{status}</span>
-        <span className="created-by">{createdBy}</span>
+        <span
+          className={
+            status === "done" || status === "cancelled" ? "closed" : "open"
+          }
+        >
+          {status === "done" || status === "cancelled" ? (
+            <GoIssueClosed />
+          ) : (
+            <GoIssueOpened />
+          )}
+          {possibleStatus.find((s) => s.id === status)?.label}
+        </span>
+        <span className="created-by">
+          {createdByUser.isLoading ? "..." : createdByUser.data.name}
+        </span>
         {"opened this issue "}
         {relativeDate(createdDate)}
-        {" . "}
+        {" · "}
         {comments.length}
         {" comments"}
       </div>
